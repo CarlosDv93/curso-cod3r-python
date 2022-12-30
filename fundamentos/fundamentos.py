59694299210b8fbca8a5a66f33e10b334e8a9074
carlosdv93 <carlosdv93@localhost.localdomain>
2022-12-30 18:09:05 -0300

    Ate a aula 28 - comentários
    Committer: carlosdv93 <carlos9_cv@hotmail.com>

---
+++ fundamentos/fundamentos.py
@@ -0,0 +1,43 @@
+print('Hello World')
+
+print('Agora, sem a maldição do hello World, vamos começar a estudar python')
+
+
+# Variáveis
+"""
+Não é tipado, é tipo dinamico, mas é fortemente tipado... 
+"""
+
+a = 2
+b = 4
+
+print(a + b)
+
+a = 'Agora sou string'
+
+print(a)
+print(b)
+# print(a+b) --> Isso aqui é ambiguo.
+
+
+# Comentários
+# Tudo que estiver depois de um '#' é considerado comentário... '#' é usado como comentário de uma única linha
+
+# Para se fazer comentário de mais de uma linha, é necessário usar '"""' o conjunto de 3 aspas simples ou duplas.
+# Vide abaixo
+
+"""
+Este é um comentário de mais de uma linha. 
+
+Pode ser colocado o que quiser aqui dentro. 
+"""
+
+'''
+Assim também funciona
+para mais de uma linha
+entendeu?
+'''
+
+print("Comentários")  # Isso aqui é ignorado
+
+# Exemplo:
